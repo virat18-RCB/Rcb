a5512a5efe248c06d3b7394f6931469f0d98d0b1
virat18-RCB <pg254954@gmail.com>
2025-04-03 15:07:54 +0530

    Update bubble_sort_&_graph.py

--- bubble_sort_&_graph.py
+++ bubble_sort_&_graph.py
@@ -1,1 +1,11 @@
-
+def sort(num):
+    for i in range(len(num)-1,0,-1):
+        for j in range(i):
+            if num[j]>num[j+1]:
+                temp=num[j]
+                num[j]=num[j+1]
+                num[j+1]=temp
+num=[18.17,1,7,6,2]
+print(num,"before bubble sort")
+sort(num)
+print(num,"after bubble sort")
