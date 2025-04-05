055fee79ba2eb4365af813a8f92d1ff833de60b3
virat18-RCB <pg254954@gmail.com>
2025-04-05 15:28:09 +0530

    Update selection_sort_&graph.py

--- selection_sort_&graph.py
+++ selection_sort_&graph.py
@@ -13,3 +13,24 @@ num=[4,18,34,3,2,1]
 print(num,"number before using the selection sort ")
 sort(num)
 print(num,"after using the selection sort")
+
+
+#### graph for selection sort::::::***********
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+n=np.linspace(1,50,50)
+best_case=n**2
+average_case=n**2
+worst_case=n**2
+plt.figure(figsize=(10,20))
+plt.plot(n,best_case,label="best_case:0(n**2)",color="red",linewidth="2")
+plt.plot(n,average_case,label="average_case:0(n**2)",c="green",linewidth='2',linestyle='-')
+plt.plot(n,worst_case,label="worst_case:o(n**2)",c="blue",linewidth='2',linestyle=":")
+plt.xlabel("input x value ")
+plt.ylabel("time complexity for selection sort")
+plt.title("graph for selection sort ")
+plt.grid(True)
+plt.legend()
+plt.show()
